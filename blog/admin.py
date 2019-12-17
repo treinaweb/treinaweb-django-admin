619e67cb7916c7fc581fcdc28f31a3be53f9c711
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2019-12-17 10:21:36 -0300

    04.01_Tabular_Inline

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class PostInline(admin.TabularInline):
+    model = Post
+
+class AutorAdmin(admin.ModelAdmin):
+    inlines = (PostInline, )
+
 admin.site.register(Post)
-admin.site.register(Autor)
+admin.site.register(Autor, AutorAdmin)
