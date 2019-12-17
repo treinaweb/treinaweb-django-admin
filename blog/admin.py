5bec4b7f506c05adb081b26e0811681f2f887eb4
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2019-12-17 11:08:50 -0300

    04.02_Stacked_Inline

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-class PostInline(admin.TabularInline):
+# class PostInline(admin.TabularInline):
+#     model = Post
+
+class PostInline(admin.StackedInline):
     model = Post
 
 class AutorAdmin(admin.ModelAdmin):
