584c3a2c50eabfdd90378a4b4f288b7850bfd8d0
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2019-12-17 10:02:50 -0300

    03.02_Listando_Post_ID

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('posts', listar_posts, name='posts')
+    path('posts', listar_posts, name='posts'),
+    path('post/<int:id>', listar_post_id, name='post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .services import post_service
 def listar_posts(request):
     posts = post_service.listar_posts()
     return render(request, 'posts.html', {'posts': posts})
+
+def listar_post_id(request, id):
+    post = post_service.listar_post_id(id)
+    return render(request, 'post.html', {'post': post})
